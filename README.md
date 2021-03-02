598212218b231b265b2d729959abad1731d31ba4
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-03-01 21:44:04 -0800

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # generate-survey-comments
+
 Generate artificial survey comments based on real data.
 
-**in early stages of development**
+Steps
 
-See *seq2seqcomments.ipynb* for model and training.
+1.  Collected sample data from multiple publicly available sources, including data.world. Shoot me a note if you would like the training data set, \~4k comments.
 
-Shoot me a note if you would like the training data set, ~4k comments. It is based off publicly available comments, mostly from *data.world*.
+2.  Combine and clean data (*compile_data.R*). Save as *data/training_comments.csv*
+
+3.  Save a copy of *training_comments.csv* to Google Drive. Location: '/content/gdrive/MyDrive/Development/seq2seqcomments/training_comments.csv'
+
+4.  Train model on Google Colab using *seq2seqcomments.ipynb*.
+
+5.  Generate comments **TBD**
--- compile_data.R
+++ compile_data.R
@@ -3,23 +3,24 @@ library(readxl)
 library(janitor)
 library(textclean)
 
-cnog <- data.frame(comment = read_lines("comments-not-on-github.txt"))
+cnog <- data.frame(comment = read_lines("data/comments-not-on-github.txt")) %>% 
+  mutate(comment = str_replace_all(comment, "\"", ""))
 
 # May need work
-csoec <- read_csv("community_survey_open_ended_comments_2016_2017_1_needswork.csv") %>% 
+csoec <- read_csv("data/community_survey_open_ended_comments_2016_2017_1_needswork.csv") %>% 
   select(comment)
 
-dwsw <- read_excel("data.world set_goodstuffwoman.xlsx") %>% 
+dwsw <- read_excel("data/data.world set_goodstuffwoman.xlsx") %>% 
   clean_names() %>% 
   pivot_longer(c(team, empowerment, gender_groups, race_groups, gender_comp, race_comps, improvements), values_to = "comment") %>% 
   select(comment)
 
-kpis <- read_csv("key-performance-indicator-surveys-1needswork.csv") %>% 
+kpis <- read_csv("data/key-performance-indicator-surveys-1needswork.csv") %>% 
   clean_names() %>% 
   select(comment = additional_feedback) %>% 
   filter(!is.na(comment), nchar(comment) > 5)
 
-sc <- read_csv("salaries_clean_ok.csv") %>% 
+sc <- read_csv("data/salaries_clean_ok.csv") %>% 
   select(comment = comments) %>% 
   filter(!is.na(comment))
 
@@ -28,4 +29,4 @@ training_comments <- bind_rows(cnog, csoec, dwsw, kpis, sc) %>%
          comment = replace_non_ascii(comment, " "),
          comment = str_trunc(comment, 1000, "right", ellipsis = ""))
   
-write_csv(training_comments, "training_comments.csv")
+write_csv(training_comments, "data/training_comments.csv", col_names = FALSE)
